44f0de492c76a9eadeb7d267ae104961df63f590
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-02-21 20:39:39 -0500

    Built-in middleware in Express

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
+
+//custom middle ware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
+// built-in middle ware
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("^/$|index(.html)?", (req, res) => {
   //res.sendFile("./views/index.html", {root: __dirname});
   res.sendFile(path.join(__dirname, "views", "index.html"));
@@ -40,7 +54,7 @@ const two = (req, res, next) => {
 
 const three = (req, res) => {
   console.log("Three");
-  res.send('Finished!')
+  res.send("Finished!");
 };
 
 app.get("/chain(.html)?", [one, two, three]);
